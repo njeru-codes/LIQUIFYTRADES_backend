e9ddc8a835087c3de3aafc924e13dd18e03caaed
bernard njeru <bernardnjerumtwaiti@gmail.com>
2022-08-30 15:56:07 +0300

    created register route

--- app/routes/register.py
+++ app/routes/register.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from ..schemas import User
 from ..database import get_db
 from sqlalchemy.orm import Session
