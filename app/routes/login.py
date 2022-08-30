9feb626a3bcfcf6a6e3860b3a2b692107b4dda3f
bernard njeru <bernardnjerumtwaiti@gmail.com>
2022-08-30 18:26:57 +0300

    edited login route

--- app/routes/login.py
+++ app/routes/login.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from ..schemas import User
+from .. import model
+from ..auth import create_access_token
 
 router = APIRouter()
 
@@ -7,4 +9,12 @@ router = APIRouter()
 
 @router.post('/login')
 async def login(user: User):
-    return user 
+    user_db = db.query( model.User).filter( model.User.email== user.email).first()
+    if not user_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"{user.email} email does not exists")
+    if not utils.verify_password(user_db.password, user.password):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='wrong password')
+    
+    access_token = create_access_token( data={"user_id": user_db.id})
+    return { "access_token": access_token, "token_type": "bearer"}
+    
