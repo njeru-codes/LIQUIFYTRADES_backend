b35fe725396fe1507f12ba5af18055b89a03b0da
bernard njeru <bernardnjerumtwaiti@gmail.com>
2022-08-31 10:07:25 +0300

    updated login

--- app/routes/login.py
+++ app/routes/login.py
@@ -14,7 +14,7 @@ async def login(user: User, db: Session=Depends(get_db)):
     user_db = db.query( model.User).filter( model.User.email== user.email).first()
     if not user_db:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"{user.email} email does not exists")
-    if not utils.verify_password(user_db.password, user.password):
+    if not utils.verify_password( user.password, user_db.password):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='wrong password')
     
     access_token = create_access_token( data={"user_id": user_db.id})
