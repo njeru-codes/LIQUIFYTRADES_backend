e4e2388f413366866dfee79dadb7e28fb2b5d2d3
unknown <bernardnjerumtwaiti@gmail.com>
2022-09-21 05:52:31 -0700

    fixed bugs and typo errors

--- app/routes/journal.py
+++ app/routes/journal.py
@@ -32,7 +32,7 @@ async def get_journal(journal_id:int, db: Session=Depends(get_db) , user_id:int 
     if not journal:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"access to journal with id {journal_id} denied")
     trades = db.query( model.Trade).filter( model.Trade.journal_id == journal.id).all()
-    return  { "date": journal.date, "id": journal.id} + trades
+    return  { "date": journal.date, "id": journal.id , "trades": trades}
 
 
 @router.delete('/{journal_id}')
