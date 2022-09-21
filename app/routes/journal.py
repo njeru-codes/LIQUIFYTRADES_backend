2cdb7ad8d8baf0e663542c4c45d4654ec2f523e7
unknown <bernardnjerumtwaiti@gmail.com>
2022-09-21 03:03:27 -0700

    fixed typo error

--- app/routes/journal.py
+++ app/routes/journal.py
@@ -31,6 +31,7 @@ async def get_journal(journal_id:int, db: Session=Depends(get_db) , user_id:int 
     journal = db.query( model.Journal).filter( model.Journal.user_id == user_id, model.Journal.id == journal_id ).first()
     if not journal:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"access to journal with id {journal_id} denied")
+    
     return journal
 
 
